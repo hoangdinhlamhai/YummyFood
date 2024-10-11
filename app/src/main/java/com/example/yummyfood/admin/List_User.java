86000b5251b60eb027a3d3cd0b6a7e6b52e69718
hoangdinhlamhai <hoangdinhlamhai1@gmail.com>
2024-10-11 17:04:42 +0700

    admin_add_user2

--- app/src/main/java/com/example/yummyfood/admin/List_User.java
+++ app/src/main/java/com/example/yummyfood/admin/List_User.java
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.yummyfood.R;
 
-public class Add_Food extends AppCompatActivity {
+public class List_User extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_add_food);
+        setContentView(R.layout.activity_list_user);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
