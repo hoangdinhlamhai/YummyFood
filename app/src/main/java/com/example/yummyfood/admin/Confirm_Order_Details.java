12d12ee06d1ecf25b15dae39e6121adc040dfe26
hoangdinhlamhai <hoangdinhlamhai1@gmail.com>
2024-10-13 18:40:55 +0700

    confirm_order_details

---
+++ app/src/main/java/com/example/yummyfood/admin/Confirm_Order_Details.java
@@ -0,0 +1,21 @@
+package com.example.yummyfood.admin;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.yummyfood.R;
+
+public class Confirm_Order_Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_confirm_order_details);
+    }
+}
