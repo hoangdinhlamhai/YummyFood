d946adb9152ed6640387772c3d1b9bc4158401f7
NguyenVanKhanh1308 <khanhvn1308@gmail.com>
2024-10-15 05:35:29 +0700

    Update FoodRetailActivity.java

--- app/src/main/java/com/example/yummyfood/FoodRetailActivity.java
+++ app/src/main/java/com/example/yummyfood/FoodRetailActivity.java
@@ -1,6 +1,9 @@
 package com.example.yummyfood;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,18 @@ public class FoodRetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_retail_user);
         // Xử lý giao diện cho FoodRetailActivity ở đây
+        // Khởi tạo nút btn_return_retailfood
+        Button btnReturnRetailFood = findViewById(R.id.btn_return_retailfood);
+
+        // Thiết lập sự kiện nhấn nút
+        btnReturnRetailFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển về HomepageUserActivity
+                Intent intent = new Intent(FoodRetailActivity.this, HomepageUserActivity.class);
+                startActivity(intent);
+                finish(); // Kết thúc FoodRetailActivity để không trở lại được bằng nút quay lại
+            }
+        });
     }
 }
