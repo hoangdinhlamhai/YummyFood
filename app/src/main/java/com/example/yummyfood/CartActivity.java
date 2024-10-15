4925c5d21a495dc88daea6b4b8d73f3478f4d592
NguyenVanKhanh1308 <khanhvn1308@gmail.com>
2024-10-15 21:12:06 +0700

    add su kien quay lai

--- app/src/main/java/com/example/yummyfood/CartActivity.java
+++ app/src/main/java/com/example/yummyfood/CartActivity.java
@@ -10,5 +10,17 @@ public class CartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
         // Xử lý giao diện cho FoodRetailActivity ở đây
+        Button btnReturnCart = findViewById(R.id.btn_return_cart);
+
+        // Thiết lập sự kiện nhấn nút
+        btnReturnCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển về HomepageUserActivity
+                Intent intent = new Intent(CartActivity.this, HomepageUserActivity.class);
+                startActivity(intent);
+                finish(); // Kết thúc FoodRetailActivity để không trở lại được bằng nút quay lại
+            }
+        });
     }
 }
