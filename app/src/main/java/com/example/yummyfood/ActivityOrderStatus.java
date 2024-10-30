99074888e2c700f110bff3a0db7299ba41fc0536
NguyenVanKhanh1308 <khanhvn1308@gmail.com>
2024-10-30 21:30:35 +0700

    Update ActivityOrderStatus.java

--- app/src/main/java/com/example/yummyfood/ActivityOrderStatus.java
+++ app/src/main/java/com/example/yummyfood/ActivityOrderStatus.java
@@ -1,6 +1,9 @@
 package com.example.yummyfood;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +12,19 @@ public class ActivityOrderStatus  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_status);
-        // Xử lý giao diện cho FoodRetailActivity ở đây
+        // Xử lý giao diện  ở đây
+        // Khởi tạo nút btn_see_history_orderstatus
+        Button btnSeeHistoryOrder = findViewById(R.id.btn_see_history_orderstatus);
+
+        // Thiết lập sự kiện nhấn nút
+        btnSeeHistoryOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển về HomepageUserActivity
+                Intent intent = new Intent(ActivityOrderStatus.this, Activity_History_Order.class);
+                startActivity(intent);
+                finish(); // Kết thúc FoodRetailActivity để không trở lại được bằng nút quay lại
+            }
+        });
     }
 }
