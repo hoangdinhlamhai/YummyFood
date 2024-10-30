338c99e1cc29780155cf7e5e69d00342546ac262
NguyenVanKhanh1308 <khanhvn1308@gmail.com>
2024-10-30 21:29:43 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/yummyfood/Activity_History_Order.java
@@ -0,0 +1,13 @@
+package com.example.yummyfood;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity_History_Order extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_oder_user);
+    }
+}
