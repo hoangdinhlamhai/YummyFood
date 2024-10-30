7c6d7941ca189352fdcc1534ef977fa3a8618182
Oanh Le <leoanh19102003@gmail.com>
2024-10-30 18:52:11 +0700

    update

--- app/src/main/java/com/example/yummyfood/book_detail_tb.java
+++ app/src/main/java/com/example/yummyfood/book_detail_tb.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,67 +12,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.widget.LinearLayout;
 
-public class booktable_usser extends AppCompatActivity {
+public class book_detail_tb extends AppCompatActivity {
 
     Dialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_booktable_usser);
+        setContentView(R.layout.activity_book_detail_tb);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        // thong bao hết ban
-        LinearLayout linearLayout = findViewById(R.id.ban1tang2);
-
-        linearLayout.setOnClickListener(new View.OnClickListener() {
+        // Nút xác nhận để hiển thị dialog
+        Button btntb = findViewById(R.id.confirm_button);
+        btntb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDialog();
             }
         });
-
-        //xem chi tiet thong tin ban
-        LinearLayout linearLayout1 = findViewById(R.id.ban1tt);
-        linearLayout1.setOnClickListener(new View.OnClickListener() {
+        // Chuyển qua chính sách đặt bàn
+        TextView txt1 = findViewById(R.id.policy);
+        txt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Khởi tạo Intent để chuyển đến detail_table
-                Intent intent = new Intent(booktable_usser.this, book_detail_tb.class);
+                Intent intent = new Intent(book_detail_tb.this, user_policy.class);
                 startActivity(intent);
             }
         });
-//ve trang chu
-        Button btnb = findViewById(R.id.btnBack);
-        btnb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(booktable_usser.this, HomepageUserActivity.class);
-                startActivity(intent);
-            }
-        });
-
 
     }
-
-    // Hàm hiển thị dialog
     private void showDialog() {
-        dialog = new Dialog(booktable_usser.this);
-        dialog.setContentView(R.layout.activity_dialog_bookedtable_user);
+        dialog = new Dialog(book_detail_tb.this);
+        dialog.setContentView(R.layout.activity_dialog_book_table);
 
 
         TextView exitButton = dialog.findViewById(R.id.dialog_exit);
         exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialog.dismiss(); // Đóng dialog
-                Intent intent = new Intent(booktable_usser.this, booktable_usser.class);
-                startActivity(intent); // Quay lại trang chủ
+                dialog.dismiss();
+                Intent intent = new Intent(book_detail_tb.this, booktable_usser.class);
+                startActivity(intent);
             }
         });
 
