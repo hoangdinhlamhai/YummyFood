ad5017ed06531973debce3e9f9e0316fdebdfc27
Oanh Le <leoanh19102003@gmail.com>
2024-11-15 14:29:41 +0700

    commit

--- app/src/main/java/com/example/yummyfood/history_table.java
+++ app/src/main/java/com/example/yummyfood/history_table.java
@@ -1,9 +1,12 @@
 package com.example.yummyfood;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class history_table extends AppCompatActivity {
@@ -17,6 +20,43 @@ public class history_table extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        Button cancelButton = findViewById(R.id.cancel_button);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                new AlertDialog.Builder(history_table.this)
+                        .setTitle("Xác nhận hủy bàn")
+                        .setMessage("Bạn muốn hủy bàn?")
+                        .setPositiveButton("Tiếp tục hủy", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                new AlertDialog.Builder(history_table.this)
+                                        .setTitle("Hủy bàn")
+                                        .setMessage("Yêu cầu hủy đang được xử lý. Vui lòng đợi phản hồi.")
+                                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                dialogInterface.dismiss();
+                                            }
+                                        })
+                                        .create()
+                                        .show();
+                            }
+                        })
+                        .setNegativeButton("Quay lại", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                dialogInterface.dismiss();
+                            }
+                        })
+                        .create()
+                        .show();
             }
         });
     }
